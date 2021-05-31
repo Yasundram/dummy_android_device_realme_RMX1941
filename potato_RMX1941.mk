fc80d613bb1e65fcd8114ef59c88f195251600f1
Anoos Ragh <83896147+cArN4gEisDeD@users.noreply.github.com>
2021-05-31 19:19:02 +0530

    Update potato_RMX1941.mk

--- potato_RMX1941.mk
+++ potato_RMX1941.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk))
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX1941/device.mk)
