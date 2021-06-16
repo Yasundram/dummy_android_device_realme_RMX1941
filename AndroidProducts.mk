4e74d8d856267b11dcb4b176b65521eac78dd483
Sundram <63789329+Yasundram@users.noreply.github.com>
2021-06-17 04:06:59 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2020 Android Open Source Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/corvus_RMX1941.mk
+    $(LOCAL_DIR)/descendant_RMX1941.mk
 
 COMMON_LUNCH_CHOICES := \
-        corvus_RMX1941-eng \
-        corvus_RMX1941-user \
-        corvus_RMX1941-userdebug
+    descendant_X00TD-eng \
+    descendant_X00TD-user \
+    descendant_X00TD-userdebug
